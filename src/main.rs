b0e73ddbb475b2f0a8826a36f38bb77b9cc33b1a
livingsilver94 <fabio0x@keemail.me>
2018-08-29 21:56:28 +0200

    Handle non-UTF8 characters

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ extern crate getopts;
 use cat::NumberingMode::{All, NonEmpty};
 use cat::{CatOptions, NumberingMode};
 use std::env;
-use std::process;
+use std::ffi::OsString;
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args: Vec<OsString> = env::args_os().skip(1).collect();
     let mut opts = getopts::Options::new();
     let opts = opts
         .optflag("A", "show-all", "equivalent to -vET")
@@ -28,51 +28,49 @@ fn main() {
             "use ^ and M- notation, except for LFD and TAB",
         )
         .optflag("h", "help", "display this help and exit");
-    let matches = opts.parse(&args).unwrap();
-    if matches.opt_present("h") {
-        println!(
-            "{}",
-            opts.usage("Usage: cat [OPTION]... [FILE]...\nConcatenate FILE(s) to standard output.")
-        );
-    } else {
-        let numbering_mode = if matches.opt_present("b") {
-            NonEmpty
-        } else if matches.opt_present("n") {
-            All
+    if let Ok(matches) = opts.parse(&args) {
+        if matches.opt_present("h") {
+            println!(
+                "{}",
+                opts.usage(
+                    "Usage: cat [OPTION]... [FILE]...\nConcatenate FILE(s) to standard output."
+                )
+            );
         } else {
-            NumberingMode::None
-        };
-        let end_char = Some("$".to_string())
-            .filter(|_| matches.opts_present(&['A'.to_string(), 'E'.to_string(), 'e'.to_string()]));
-        let tab_char = Some("^I".to_string())
-            .filter(|_| matches.opts_present(&['A'.to_string(), 'T'.to_string(), 't'.to_string()]));
-        let options = CatOptions {
-            numbering_mode,
-            end_char,
-            squeeze_blank: matches.opt_present("s"),
-            tab_char,
-            show_nonprinting: matches.opts_present(&[
-                'A'.to_string(),
-                'v'.to_string(),
-                'e'.to_string(),
-                't'.to_string(),
-            ]),
-        };
-        let mut files: Vec<&str> = matches.free.iter().map(|x| &x[..]).collect();
-        if files.is_empty() {
-            files.push(&"-");
-        }
-        if let Err(error) = cat::concat(&options, &files) {
-            match error.get_ref() {
-                Some(inner) => {
-                    eprintln!("{}", inner);
-                    process::exit(1);
-                }
-                None => {
-                    eprintln!("{}", error);
-                    process::exit(error.raw_os_error().unwrap());
-                }
+            let numbering_mode = if matches.opt_present("b") {
+                NonEmpty
+            } else if matches.opt_present("n") {
+                All
+            } else {
+                NumberingMode::None
+            };
+            let end_char = Some("$".to_string()).filter(|_| {
+                matches.opts_present(&['A'.to_string(), 'E'.to_string(), 'e'.to_string()])
+            });
+            let tab_char = Some("^I".to_string()).filter(|_| {
+                matches.opts_present(&['A'.to_string(), 'T'.to_string(), 't'.to_string()])
+            });
+            let options = CatOptions {
+                numbering_mode,
+                end_char,
+                squeeze_blank: matches.opt_present("s"),
+                tab_char,
+                show_nonprinting: matches.opts_present(&[
+                    'A'.to_string(),
+                    'v'.to_string(),
+                    'e'.to_string(),
+                    't'.to_string(),
+                ]),
+            };
+            let mut files: Vec<&str> = matches.free.iter().map(|x| &x[..]).collect();
+            if files.is_empty() {
+                files.push(&"-");
+            }
+            if let Err(error) = cat::concat(&options, &files) {
+                eprintln!("{}", error);
             }
         }
+    } else {
+        eprintln!("Cannot parse arguments.");
     }
 }
